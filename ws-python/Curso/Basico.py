c308aa3688a2140cb8cf2370bd3a974b715c659c
LuAgostinho <lucasagostinhosilv@gmail.com>
2025-07-24 15:52:36 -0300

    Ajustando nomenclatura

---
+++ ws-python/Curso/Basico.py
@@ -0,0 +1,17 @@
+#Tipos padrões de variáveis
+Numero_inteiro: int
+Numero_com_virgula: float
+Caracteres: str
+
+#Comando para printar mensagens no console
+print("Ola mundo")
+
+Caracteres = "Teste de print"
+Numero_inteiro = 2
+Numero_com_virgula = 3.1456
+
+#Print de variaveis
+print(f"Teste = {Caracteres} \n")
+print(f"Numero_inteiro = {Numero_inteiro}\n")
+#Print de variável com limite de casas decimais
+print(f"Numero_com_virgula = {Numero_com_virgula:.2f}")
